11b1f43d6bc844928953b3a5a828925bb6313e80
Egorov Anton <egorov@gett.com>
2018-12-14 12:14:59 +0400

    add methods

--- controllers/default.go
+++ controllers/default.go
@@ -11,3 +11,11 @@ type MainController struct {
 func (c *MainController) Get() {
 	c.TplName = "index.html"
 }
+
+func (c *MainController) Head() {
+	c.CustomAbort(200, "")
+}
+
+func (c *MainController) Options() {
+	c.CustomAbort(200, "")
+}
